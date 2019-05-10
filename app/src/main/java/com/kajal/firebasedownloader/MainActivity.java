0d880a8a671905349a3622ab6e5b5f3fa3b4b374
saikat <saikatmondal410@gmail.com>
2019-05-10 18:43:05 +0530

    1st commit,till now the app downloads 2019.pdf from firebase and stores in Downloads dir

---
+++ app/src/main/java/com/kajal/firebasedownloader/MainActivity.java
@@ -0,0 +1,74 @@
+package com.kajal.firebasedownloader;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button Down;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    StorageReference ref;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Down = findViewById(R.id.down);
+
+        Down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                download();
+            }
+        });
+    }
+
+    public void download(){
+        storageReference = firebaseStorage.getInstance().getReference();
+        ref = storageReference.child("2019.pdf");
+
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url = uri.toString();
+                downloadFiles(MainActivity.this,"2019",".pdf",
+                        DIRECTORY_DOWNLOADS,url);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+
+    public void downloadFiles(Context context,String fileName,String fileExtension,
+                              String destinationDirectory, String url){
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory,fileName+fileExtension);
+        downloadManager.enqueue(request);
+    }
+}
